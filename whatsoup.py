ed82eb08a6d656dacabfa32807c0059e20311736
Eddy Harrington <hisfastness@outlook.com>
2021-02-01 17:00:17 -0800

    Add chat list scrape

--- whatsoup.py
+++ whatsoup.py
@@ -24,6 +24,9 @@ def main():
         print("You've quit WhatSoup.")
         return
 
+    # Get chats
+    chats = get_chats(driver)
+
 
 def setup_selenium():
     '''Setup Selenium to use Chrome webdriver'''
@@ -91,6 +94,113 @@ def user_is_logged_in(driver, wait_time):
         return True
     except TimeoutException:
         return False
+
+
+def get_chats(driver):
+    '''Traverses the WhatsApp chat-pane via keyboard input and collects chat information such as person/group name, last chat time and msg'''
+
+    # Find the chat search input because the element below it is always the most recent chat
+    chat_search = driver.find_element_by_xpath(
+        '//*[@id="side"]/div[1]/div/label/div/div[2]')
+    chat_search.click()
+
+    # Count how many chat records there are below the search input by using keyboard navigation because HTML is dynamically changed depending on viewport and location in DOM
+    selected_chat = driver.switch_to.active_element
+    prev_chat_id = None
+    is_last_chat = False
+    chats = []
+
+    # Descend through the chats
+    while True:
+        # Navigate to next chat
+        selected_chat.send_keys(Keys.DOWN)
+
+        # Set active element to new chat (without this we can't access the elements '.text' value used below for name/time/msg)
+        selected_chat = driver.switch_to.active_element
+
+        # Check if we are on the last chat by comparing current to previous chat
+        if selected_chat.id == prev_chat_id:
+            is_last_chat = True
+        else:
+            prev_chat_id = selected_chat.id
+
+        # Gather chat info (chat name, chat time, and last chat message)
+        if is_last_chat:
+            break
+        else:
+            # TODO refactor this area later, there have been a few intermittent issues with odd text splits due to inconsistent HTML
+            # based on individual/group chats, emojis, attachments, etc. Should use BS4 as it grants more flexibility for slicing HTML.
+
+            chat_info = selected_chat.text.splitlines()
+
+            # One-on-one chats: chat name, last chat time, last chat msg
+            if len(chat_info) == 3:
+                name_of_chat = chat_info[0]
+                last_chat_time = chat_info[1]
+                last_chat_msg = chat_info[2]
+            # Group chats: chat name, last chat time, name of last msg sender, last chat msg
+            elif len(chat_info) == 5:
+                # Note: ignore item3 which is always ':' in group chat
+                name_of_chat = chat_info[0]
+                last_chat_time = chat_info[1]
+                last_chat_msg = f"{chat_info[2]}: {chat_info[4]}"
+            # Edge cases
+            else:
+                # One-on-one chat where last message is an emoji. Splits the elements text w/ items 0) name of sender, 2) last chat time
+                if len(chat_info) == 2:
+                    try:
+                        name_of_chat = chat_info[0]
+                        last_chat_time = chat_info[1]
+
+                        # TODO below only grabs the first emoji item. If there are many emojis we need to find
+                        # all child elements and build a single string of the text/emojis.
+
+                        # Make sure to scrape from the chat preview span (class '_7W_3c') and not username span (class '_1c_mC')
+                        last_chat_msg = selected_chat.find_element_by_class_name(
+                            '_7W_3c').find_element_by_class_name('emoji').get_attribute('alt')
+                    except NoSuchElementException:
+                        print(
+                            f"Something went wrong while reading a chat card. Skipping '{selected_chat.text}'")
+                        print(f"Chat card info: {chat_info}")
+                        continue
+
+                # One-on-one chat where last message is a photo attachment OR Group chat where last message is an emoji.
+                # TODO: Intermittent issue only happened 2 or 3 times...One-on-one implementation: splits the elements text w/ items 0) name of group, 1), last chat time, 2) 'Photo', 3) '1'
+
+                # Group chat implementation: splits the elements text w/ items 0) name of group, 1) last chat time, 2) sender, 3) ': '
+                elif len(chat_info) == 4:
+                    try:
+                        name_of_chat = chat_info[0]
+                        last_chat_time = chat_info[1]
+                        # Make sure to scrape from the chat preview span (class '_7W_3c') and not username span (class '_1c_mC')
+                        emoji_loc = selected_chat.find_element_by_class_name(
+                            '_7W_3c').find_element_by_class_name('emoji').get_attribute('alt')
+
+                        # Build the entire message by combining text w/ emoji
+                        last_chat_msg = f"{chat_info[2]}{chat_info[3].strip()} {emoji_loc}"
+                    except NoSuchElementException:
+                        print(
+                            f"Something went wrong while reading a chat card. Skipping '{selected_chat.text}'")
+                        print(f"Chat card info: {chat_info}")
+                        continue
+
+                # Handle any other length in case of errors
+                else:
+                    print(
+                        f"Something went wrong while reading a chat card. Skipping '{selected_chat.text}'")
+                    print(f"Chat card info: {chat_info}")
+                    continue
+
+            # Store chat info within a dict
+            chat = {"name": name_of_chat,
+                    "time": last_chat_time, "message": last_chat_msg}
+            chats.append(chat)
+
+    # Navigate back to the top of the chat list
+    chat_search.click()
+    chat_search.send_keys(Keys.DOWN)
+
+    return chats
 
 
 if __name__ == "__main__":
